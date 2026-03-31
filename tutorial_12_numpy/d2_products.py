23ab69a79adb5257219ec5c8f4d160b0a406f896
Arun Kumar <java.kumar.arun@gmail.com>
2026-03-31 11:51:17 +0530

    31march

---
+++ tutorial_12_numpy/d2_products.py
@@ -0,0 +1,14 @@
+import numpy
+
+items = [99, 105, 81, 110, 25]
+list = numpy.array(items)
+
+# print("items:",type(items))
+# print("numpy list:",type(list))
+# print(items)
+# print(list)
+
+total = list.sum()
+print(total)
+average =list.mean()
+print(average)
